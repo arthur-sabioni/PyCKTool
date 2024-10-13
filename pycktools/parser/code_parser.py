f874055d85a14a79c52b2afc33711cfbd2efc289
unknown <arthur.lsabioni@gmail.com>
2024-10-13 18:18:04 -0300

    Changes in attributes and methods usage detection logic

--- pycktools/parser/code_parser.py
+++ pycktools/parser/code_parser.py
@@ -1,23 +1,31 @@
 import astroid
 import json
 
-class SetEncoder(json.JSONEncoder):
-    def default(self, obj):
-        if isinstance(obj, set):
-            return list(obj)
-        return json.JSONEncoder.default(self, obj)
+from utils.json_utils import SetEncoder
 
 class CodeParser:
+
+    _CLASS_INIT = {
+        'accessed_attributes': set(),
+        'called': set(),
+        'methods': {},
+        'attributes': set(),
+        'variables': set(),
+        'direct_children': 0,
+        'coupled_classes': set()
+    }
+
+    _METHOD_INIT = {
+        'accessed_attributes': set(),
+        'called': set(),
+        'lloc': 0,
+        'number_of_parameters': 0,
+    }
     
     def __init__(self) -> None:
 
         self.classes = {}
-        self.methods = {}
         self.inheritances = {}
-        
-    def get_class(self, class_name) -> dict:
-        
-        return self.classes.get(class_name, self._CLASS_INIT)
 
     def count_lloc(self, node):
         """
@@ -28,9 +36,11 @@ class CodeParser:
         
         lloc = 0
         for child in node.body:
-            if isinstance(child, (astroid.Assign, astroid.AugAssign, astroid.AnnAssign, astroid.Expr, astroid.Return,
-                                astroid.Raise, astroid.Delete, astroid.Pass, astroid.Break, astroid.Continue,
-                                astroid.Global, astroid.Nonlocal, astroid.Assert, astroid.Import, astroid.ImportFrom)):
+            if isinstance(child, (
+                astroid.Assign, astroid.AugAssign, astroid.AnnAssign, astroid.Expr, astroid.Return,
+                astroid.Raise, astroid.Delete, astroid.Pass, astroid.Break, astroid.Continue,
+                astroid.Global, astroid.Nonlocal, astroid.Assert, astroid.Import, astroid.ImportFrom
+            )):
                 lloc += 1
             elif isinstance(child, (astroid.If, astroid.For, astroid.While, astroid.Try, astroid.With)):
                 lloc += self.count_lloc_in_compound_statement(child)
@@ -42,118 +52,155 @@ class CodeParser:
         """
         lloc = 1  # Count the compound statement itself
         for child in node.body:
-            if isinstance(child, (astroid.Assign, astroid.AugAssign, astroid.AnnAssign, astroid.Expr, astroid.Return,
-                                astroid.Raise, astroid.Delete, astroid.Pass, astroid.Break, astroid.Continue,
-                                astroid.Global, astroid.Nonlocal, astroid.Assert, astroid.Import, astroid.ImportFrom)):
+            if isinstance(child, (
+                astroid.Assign, astroid.AugAssign, astroid.AnnAssign, astroid.Expr, astroid.Return,
+                astroid.Raise, astroid.Delete, astroid.Pass, astroid.Break, astroid.Continue,
+                astroid.Global, astroid.Nonlocal, astroid.Assert, astroid.Import, astroid.ImportFrom
+            )):
                 lloc += 1
             elif isinstance(child, (astroid.If, astroid.For, astroid.While, astroid.Try, astroid.With)):
                 lloc += self.count_lloc_in_compound_statement(child)
         for child in getattr(node, 'orelse', []):
-            if isinstance(child, (astroid.Assign, astroid.AugAssign, astroid.AnnAssign, astroid.Expr, astroid.Return,
-                                astroid.Raise, astroid.Delete, astroid.Pass, astroid.Break, astroid.Continue,
-                                astroid.Global, astroid.Nonlocal, astroid.Assert, astroid.Import, astroid.ImportFrom)):
+            if isinstance(child, (
+                astroid.Assign, astroid.AugAssign, astroid.AnnAssign, astroid.Expr, astroid.Return,
+                astroid.Raise, astroid.Delete, astroid.Pass, astroid.Break, astroid.Continue,
+                astroid.Global, astroid.Nonlocal, astroid.Assert, astroid.Import, astroid.ImportFrom
+            )):
                 lloc += 1
             elif isinstance(child, (astroid.If, astroid.For, astroid.While, astroid.Try, astroid.With)):
                 lloc += self.count_lloc_in_compound_statement(child)
         return lloc
     
-    def extract_attributes_recursive(self, node, dict, class_name) -> None:
-        if isinstance(node.value, astroid.Call):
-            if node.value.args:
-                for arg in node.value.args:
-                    self.extract_attributes_recursive(arg, dict, class_name)
-            called_method = node.value.func.as_string()
-            node['called_methods'].add(called_method)
+    def _extract_used_attributes_recursive(self, node, dict, class_name) -> None:
+        """
+        Recursively traverse the node and extract the methods that are called and the attributes that are accessed. 
+        The extracted data is stored in the given dictionary.
+        """
+        if isinstance(node, astroid.Call):
+            if node.args:
+                for arg in node.args:
+                    self._extract_used_attributes_recursive(arg, dict, class_name)
+            called_method = node.func.as_string()
+            dict['called'].add(called_method)
             if '.' in called_method:
                 callee_class = called_method.split('.')[0]
                 if callee_class != class_name:
                     self.classes[class_name]['coupled_classes'].add(callee_class)
+        if isinstance(node, astroid.Attribute):
+            attr_name = node.attrname
+            if node.expr.as_string() != 'self':
+                used_class = node.expr.as_string()
+                self.classes[class_name]['coupled_classes'].add(used_class)
+                attr_name = used_class + '.' + attr_name
+            dict['accessed_attributes'].add(attr_name)
+
+    def _extract_self_attributes(self, node, dict, class_name) -> None:
+        """
+        Extract the attributes of the class that are accessed via the 'self' variable
+          from the given node and store the extracted data in the given dictionary.
+        """
+        for target in node.targets:
+            if isinstance(target, astroid.AssignAttr):
+                attr_name = target.attrname
+                attr_instance = next(target.infer(), None).pytype()
+                if attr_instance is astroid.Uninferable:
+                    attr_instance = None
+                else:
+                    if attr_instance[0] == '.':
+                        attr_instance = attr_instance[1:]
+                self.classes[class_name]['attributes'].add((
+                    attr_name, attr_instance
+                ))
+                dict['accessed_attributes'].add(attr_name)
         
-    def extract_methods(self, node: astroid.FunctionDef, class_name: str) -> None:
+    def _extract_methods(self, node: astroid.FunctionDef, class_name: str) -> None:
         
+        """
+        Extract the methods of the class from the given node and store the extracted data in the 
+        classes dictionary.
+
+        The extracted data includes the name of the method, the logical lines of code (LLOC), the number of
+        parameters, the attributes that are accessed and the methods that are called.
+        """
         method_name = node.name
-        method_dict = {
-            'accessed_attributes': set(),
-            'called_methods': set(),
-            'lloc': self.count_lloc(node),
-            'number_of_parameters': len(node.args.args),
-        }
+        method_dict = self._METHOD_INIT
+        method_dict['lloc'] = self.count_lloc(node)
+        method_dict['number_of_parameters'] = len(node.args.args)
         
         for method_node in node.body:
             if isinstance(method_node, astroid.Assign):
-                for target in method_node.targets:
-                    if isinstance(target, astroid.AssignAttr):
-                        attr_name = target.attrname
-                        self.classes[class_name]['attributes'].add(attr_name)
-                        method_dict['accessed_attributes'].add(attr_name)
+                self._extract_self_attributes(method_node, method_dict, class_name)
             
-            elif isinstance(method_node, astroid.Expr):
-                # self.extract_attributes_recursive(method_node, method_dict, class_name)
-                if isinstance(method_node.value, astroid.Call):
-                    if method_node.value.args:
-                        for arg in method_node.value.args:
-                            arg_string = arg.as_string().replace('self.', '')
-                            if '.' in arg_string:
-                                callee_class = arg_string.split('.')[0]
-                                if callee_class != class_name:
-                                    self.classes[class_name]['coupled_classes'].add(callee_class)
-                            method_dict['accessed_attributes'].add(arg_string)
-                    called_method = method_node.value.func.as_string().replace('self.', '')
-                    method_dict['called_methods'].add(called_method)
-                    if '.' in called_method:
-                        callee_class = called_method.split('.')[0]
-                        if callee_class != class_name:
-                            self.classes[class_name]['coupled_classes'].add(callee_class)
+            if isinstance(method_node, (astroid.Expr, astroid.Assign)):
+                self._extract_used_attributes_recursive(
+                    method_node.value, method_dict, class_name
+                )
 
         self.classes[class_name]['methods'][method_name] = method_dict
                             
-    def extract_inheritance(self, base: astroid.FunctionDef, class_name: str) -> None:
+    def _extract_inheritance(self, base: astroid.FunctionDef, class_name: str) -> None:
         base_class = base.name
         self.inheritances[class_name] = base_class
         if base_class in self.classes:
             self.classes[base_class]['direct_children'] += 1
 
-    def extract_classes_and_methods(self, code: str) -> None:
+    def _extract_classes_data(self, module: astroid.Module) -> None:
         
-        module = astroid.parse(code)
-
         for node in module.body:
             if isinstance(node, astroid.ClassDef):
                 class_name = node.name
-                self.classes[class_name] = {
-                    'methods': {},
-                    'attributes': set(),
-                    'variables': set(),
-                    'direct_children': 0,
-                    'coupled_classes': set()
-                }
+                self.classes[class_name] = self._CLASS_INIT
 
                 # Extract methods and attributes
                 for class_node in node.body:
-                    if isinstance(class_node, astroid.FunctionDef):
-                        self.extract_methods(class_node, class_name)
 
-                    elif isinstance(class_node, astroid.Assign):
+                    # Method instantiation
+                    if isinstance(class_node, astroid.FunctionDef):
+                        self._extract_methods(class_node, class_name)
+
+                    # Attribute assign
+                    if isinstance(class_node, astroid.Assign):
                         for target in class_node.targets:
                             attr_name = target.name
                             self.classes[class_name]['variables'].add(attr_name)
+                            
+                    # Method call or attribute access
+                    if isinstance(class_node, (astroid.Expr, astroid.Assign)):
+                        self._extract_used_attributes_recursive(
+                            class_node.value, class_node, class_name
+                        )
 
                 # Extract inheritance information
                 for base in node.bases:
                     if isinstance(base, astroid.Name):
-                        self.extract_inheritance(base, class_name)
+                        self._extract_inheritance(base, class_name)
+
+    def _translate_attributes_to_classes(self) -> None:
+        pass
+        # for class_name, class_data in self.classes.items():
+        #     for method, method_data in class_data['methods'].items():
+        #         for attr in method_data['accessed_attrigutes']:
+        #             if attr in self.classes.keys():
+        #                 self.classes[class_name]['coupled_classes'].add(attr_type)
+
+    def extract_code_data(self, code: str) -> None:
+        
+        module = astroid.parse(code)
+
+        self._extract_classes_data(module)
+        self._translate_attributes_to_classes()
 
 
 # Test execution
 if __name__ == "__main__":
     cp = CodeParser()
-    cp.extract_classes_and_methods(r"""
+    cp.extract_code_data(r"""
                                    
         class UsedClass:
             def __init__(self):
                 self.used_attr_1 = 'I am here to be used!'
                 self.used_attr_2 = 'I am here to be used!'
-                self.used_attr_3 = 'I am here to be used!'
+                self.used_attr_3 = UsedClass()
         
         class MySuperParent:
             super_parent_var = 0
@@ -168,16 +215,17 @@ if __name__ == "__main__":
                 pass
 
         class MyClass(MyParent):
-            class_variable = 0
+            class_variable = UsedClass6()
 
-            def __init__(self, value):
+            def __init__(self, value: str):
                 self.instance_variable = value
-                self.UsedClass = UsedClass()
+                self.UsedClassAttribute = UsedClass()
+                usedClassVariable = UsedClass()
 
             def my_method(self):
                 print(self.instance_variable)
-                print(self.UsedClass.used_attr)
-                print(self.UsedClass.used_attr_2, self.UsedClass.used_attr_3)
+                print(self.UsedClassAttribute.used_attr)
+                print(self.UsedClassAttribute.used_attr_2, self.UsedClassAttribute.used_attr_3)
 
         def my_function():
             local_variable = 10
